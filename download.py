7eff3cd5ff1b5846d57bbf3598a33d7e1e3e4e4a
Ben Koch <bekoch@gmail.com>
2017-05-28 14:06:01 +0200

    cleaned up

--- download.py
+++ download.py
@@ -1,3 +1,5 @@
+#! python2
+# download.py - Downloads every single installed pip package.
 
 import requests, os, bs4
 import unicodedata
@@ -17,6 +19,7 @@ def download_package(url='https://pypi.python.org/pypi/exchangelib/1.9.3'):
 
         if not 'Not Found' in soup.text:
 
+            # Find the URL of the packages.
             tables = soup.findAll('table')
 
             files = []
@@ -39,13 +42,16 @@ def download_package(url='https://pypi.python.org/pypi/exchangelib/1.9.3'):
                     try:
                         urlforfilesystem = unicodedata.normalize('NFKD', u''+ f +'').encode('ascii', 'ignore')
                         urlforfilesystem = urlforfilesystem[:urlforfilesystem.find('#')]
+                        # Download the file.
                         print('Downloading image %s...' % (urlforfilesystem))
                         res = requests.get(f)
                         res.raise_for_status()
 
+                        # Save the file to ./backup.
                         BackupFile = open(os.path.join('backup', os.path.basename(urlforfilesystem)), 'wb')
                         for chunk in res.iter_content(100000):
                             BackupFile.write(chunk)
+                        BackupFile.close()
 
                     except requests.exceptions.MissingSchema:
                         print('didnt found!')
